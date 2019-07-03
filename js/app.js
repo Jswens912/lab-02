710ad7014088ca85888e2a521cf1f40b41e895cd
jswens912 <jswens912@gmail.com>
2019-07-03 15:59:17 -0500

    add file setup

---
+++ js/app.js
@@ -0,0 +1,24 @@
+'use strict'
+
+//Ask the firt question
+var firstquestion = prompt("Was I born in Iowa?");
+if (firstquestion = Y, firstquestion = y, firstquestion = YES, firstquestion = yes);{
+     prompt("That is correct!");
+  else {
+     prompt("Nah ma dude");
+ }
+ 
+//Ask Second Question
+var secondquestion = prompt("Have I lived in Iowa my whole life?");
+if (secondquestion = N, secondquestion= n, secondquestion = NO, secondquestion = no){
+   prompt("You're right!");
+} else {
+    prompt("Yeah, that's gonna be a no for me dawg.");
+}
+var thirdquestion = prompt("Did I go to college out of state?")
+if (thirdquestion = Y, thirdquestion = YES, thirdquestion = y, thirdquestion = yes) {
+    
+} else {
+    prompt("...no.")
+    
+}
